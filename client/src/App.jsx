da197e8a783446bd5c6f7ca259c2f96c74e4f4a3
futurebound <mitchellm1618@gmail.com>
2025-01-26 15:59:08 -0800

    feat(client): axios request hitting local backend

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,7 +1,18 @@
-import {} from 'react'
+import { useEffect } from 'react'
+import axios from 'axios'
 import './App.css'
 
 function App() {
+  const fetchAPI = async () => {
+    const response = await axios.get('http://localhost:8080/api')
+    console.log(response.data.message)
+  }
+
+  // [] to ensure only runs on initial render
+  useEffect(() => {
+    fetchAPI()
+  }, [])
+
   return (
     <>
       <h1>FutureCRM</h1>
