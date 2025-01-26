ae5584f807b38f75849a65e508a2ae641482b3d2
futurebound <mitchellm1618@gmail.com>
2025-01-26 15:49:20 -0800

    chore: rename directories

--- server/src/server.js
+++ server/src/server.js
@@ -4,6 +4,16 @@ dotenv.config()
 const express = require('express')
 const app = express()
 
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
+
+/**
+ *  ---------------- ROUTES ---------------
+ */
+app.get('/api', (req, res) => {
+  res.json({ message: 'get /api/' })
+})
+
 /**
  *  ---------------- SERVER ---------------
  */
