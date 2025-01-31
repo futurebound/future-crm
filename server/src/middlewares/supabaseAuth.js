6e583f07c4f41f6e42520d06bfa62a4f04b805fb
futurebound <mitchellm1618@gmail.com>
2025-01-31 15:38:29 -0800

    chore: cleanup console logs

--- server/src/middlewares/supabaseAuth.js
+++ server/src/middlewares/supabaseAuth.js
@@ -1,13 +1,13 @@
 const supabase = require('../utils/supabaseClient')
 
 const authenticateUser = async (req, res, next) => {
-  console.log('attempting to authenticate user token')
+  // console.log('attempting to authenticate user token')
   const authHeader = req.headers.authorization
   if (!authHeader) {
     return res.status(401).json({ message: 'Unauthorized' })
   }
 
-  console.log('auth header present, extracting token')
+  // console.log('auth header present, extracting token')
   const token = authHeader.split(' ')[1]
   if (!token) {
     return res.status(401).json({ error: 'Unauthorized' })
@@ -15,7 +15,7 @@ const authenticateUser = async (req, res, next) => {
 
   try {
     // Verify the JWT using Supabase
-    console.log('attempting supabase auth with token:', token)
+    // console.log('attempting supabase auth with token:', token)
     const { data, error } = await supabase.auth.getUser(token)
     if (error) {
       throw error
